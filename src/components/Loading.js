b3bb78ff247edcdbc7370d26d9cb68503246cd9a
CRMyers72 <91919116+CRMyers72@users.noreply.github.com>
2022-05-08 09:08:55 -0400

    Art Collector project

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+//   Create and export a component called Loading which uses this static HTML template:
+function Loading(props) {
+  return (
+    <div id="loading">
+      <h2 className="message">Searching...</h2>
+    </div>
+  );
+}
+export default Loading;
